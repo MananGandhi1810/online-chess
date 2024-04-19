e1f93a72787a2080cc6407d06da481c067d4fdde
Manan Gandhi <ardumanan@gmail.com>
2024-04-19 23:48:08 +0530

    Added Storage Service

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,1 +1,17 @@
-class Stor
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class StorageService {
+  FlutterSecureStorage _storage = FlutterSecureStorage();
+
+  Future<void> write(String key, String value) async {
+    await _storage.write(key: key, value: value);
+  }
+
+  Future<String?> read(String key) async {
+    return await _storage.read(key: key);
+  }
+
+  Future<void> delete(String key) async {
+    await _storage.delete(key: key);
+  }
+}
