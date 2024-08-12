875cfc3687d164a55f8dffbdbd39553ad0598828
Manan Gandhi <ardumanan@gmail.com>
2024-08-12 23:04:50 +0530

    Refactor: IO -> io

--- lib/services/socket_service.dart
+++ lib/services/socket_service.dart
@@ -1,15 +1,15 @@
 import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
-import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'package:socket_io_client/socket_io_client.dart' as io;
 
 class SocketService {
-  IO.Socket? socket;
+  io.Socket? socket;
 
   bool get isConnected => socket?.connected ?? false;
 
   void connect(String baseUrl) {
-    socket = IO.io(baseUrl, <String, dynamic>{
+    socket = io.io(baseUrl, <String, dynamic>{
       "transports": ["websocket"],
     });
     socket?.connect();
