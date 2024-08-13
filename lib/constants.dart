11942c6b547458e4bcd68745f1f2dab7bd26ef45
Manan Gandhi <ardumanan@gmail.com>
2024-08-13 12:37:53 +0530

    Refactor: changed server URL

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,10 +1,10 @@
 class Constants {
-  // static const String domain = 'chess-server.manangandhi.tech';
-  // static const String httpBaseUrl = 'https://$domain/rest';
-  // static const String socketBaseUrl = 'https://$domain';
-  // static const String stockfishBaseUrl = 'https://$domain/stockfish';
-  static const String domain = 'localhost';
-  static const String httpBaseUrl = 'http://$domain:3000';
-  static const String socketBaseUrl = 'http://$domain:4000';
-  static const String stockfishBaseUrl = 'http://$domain:5000';
+  static const String domain = 'chess-server.manangandhi.tech';
+  static const String httpBaseUrl = 'https://$domain/rest';
+  static const String socketBaseUrl = 'https://$domain';
+  static const String stockfishBaseUrl = 'https://$domain/stockfish';
+  // static const String domain = 'localhost';
+  // static const String httpBaseUrl = 'http://$domain:3000';
+  // static const String socketBaseUrl = 'http://$domain:4000';
+  // static const String stockfishBaseUrl = 'http://$domain:5000';
 }
